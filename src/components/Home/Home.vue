b201c3465ecbbd913c4a1a83d2c0430df0829de7
amdfans9801 <wenqing8215756@163.com>
2022-09-06 00:55:02 +0800

    2022.09.06临睡前保存提交

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -7,17 +7,7 @@
 <script setup>
     import systeminfo from '../SystemInfo/SystemInfo.vue'
 
-    // var http = require("fs")
-    // console.log(http)
 
-    // const process = require('process');
-    // console.log(process)
-
-    // const diskinfo = require('diskinfo');
-    // diskinfo.getDrives((err, aDrives) => {
-    //     console.log(err);
-    //     console.log(aDrives);
-    // });
 </script>
 
 <style scoped>
---
+++ src/components/Tools/MessageBox.js
@@ -0,0 +1,6 @@
+/**
+ * 基于Element-Plus的信息提示，包括信息框，弹出的提示和tppltip等
+ * author: yanwenqing
+ * date: 2022.09.06
+ * version: 1.0.0
+ */
