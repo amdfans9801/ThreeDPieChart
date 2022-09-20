7ab7bfb01132708aa9516cba56954efc0276f5f5
amdfans9801 <wenqing8215756@163.com>
2022-09-20 23:17:24 +0800

    2022.09.20继续工作  :(

--- src/components/Home/MainView.vue
+++ src/components/Home/MainView.vue
@@ -1,13 +1,29 @@
 <template>
     <div id="mainview">
+        <div id="chartContainer">
+            <div id="threedpiechartdom"></div>
+        </div>
         
     </div>
 </template>
 
 <script setup>
+    import { ref, reactive, onMounted } from 'vue';
+    import threedpiechart from '../ThreeDPieChart/handler/threedpiechart';
 
+    const piechartdom = document.getElementById('threedpiechartdom');
+    const mychart = echarts.init(piechartdom); 
+    
+
+    onMounted(() => {
+        threedpiechart.get3DPieOption();
+    });
+
+    function setChartData(){
+        chartOption
+    }
 </script>
 
 <style scoped>
-
+    @import url(./css/mainview.css);
 </style>
