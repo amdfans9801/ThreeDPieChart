538042acef7d0abd4c62807274fa9a624fd49c3c
yanwenqing <1091603989@qq.com>
2022-10-21 17:16:27 +0800

    地图和主要内容的样式

--- src/components/MapGIS/handler/CesiumVIewer.js
+++ src/components/MapGIS/handler/CesiumVIewer.js
@@ -20,8 +20,10 @@ function initCesium(container){
         timeline: false,        //是否显示时间线控件
         showRenderLoopErrors: true,     //如果设置为true，发生渲染循环错误时，将自动给用户显示一个包含错误信息的HTML面板。
         navigation: false,      //是否显示导航罗盘控件
-        scene3DOnly: false,     //每个几何实例将只能以3D渲染以节省GPU内存
         navigationHelpButton: false,    //是否显示帮助信息控件
+        navigationInstructionsInitiallyVisible: false,      //底部的文字介绍
+        blurActiveElementOnCanvasFocus: false,
+        scene3DOnly: false,     //每个几何实例将只能以3D渲染以节省GPU内存
         timeline: false,        //是否显示时间线控件
         fullscreenButton: false,        //是否显示全屏按钮
         geocoder: false,        //是否显示地名查找控件
@@ -32,9 +34,17 @@ function initCesium(container){
         fullscreenElement: document.body,       //全屏时渲染的HTML元素 暂时没发现用处
         shadows: true,
         
-
+        
     });
+
+    viewer.scene.postProcessStages.fxaa.enabled = false;    //去除锯齿 新版本
+    viewer.scene.globe.depthTestAgainstTerrain = true;      //开启深度测试功能
+
+    // viewer.creditContainer.destroy();
+
 }
+
+
 
 
 
